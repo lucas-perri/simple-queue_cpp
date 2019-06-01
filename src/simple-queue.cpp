c2cca04ecc413b056eacadda1602502c807c93bb
Lucas Perri <lucas.perri@philips.edu.ar>
2019-05-31 23:26:56 -0300

    Progress Write Node class

--- src/simple-queue.cpp
+++ src/simple-queue.cpp
@@ -1,1 +1,40 @@
+class Node
+{
+private:
+
+    // Value stored in the node
+    int _value;
+
+    // Pointer to the next node
+    Node *_next;
+
+    // Pointer to the previous node
+    Node *_prev;
+
+public:
+
+    // Returns the value of the node
+    const int value(){ return this->_value; }
+
+    // Returns a pointer to the next node
+    Node* next(){ return this->next; }
+
+    // Returns a pointer to the previous node
+    Node* prev(){ return this->_prev; }
+
+    // Set the pointer to the next node
+    void setNext(Node *next){ this->_next = next; }
+
+    // Set the pointer to the previous node
+    void setPrev(Node *prev){ this->_prev = prev; }
+
+    Node(int value, Node *next, Node *prev)
+    {
+        this->_value = value;
+        this->_next = next;
+        this->_prev = prev;
+    }
+    ~Node();
+};
+
 int main(){}
