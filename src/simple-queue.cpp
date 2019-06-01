ff6805f4fd2c57b1b07348f831f07ec79b59af74
Lucas Perri <lucas.perri@philips.edu.ar>
2019-06-01 12:40:11 -0300

    Define class Queue Allow interaction between nodes

--- src/simple-queue.cpp
+++ src/simple-queue.cpp
@@ -15,6 +15,9 @@ private:
     // Pointer to the previous node
     Node *_prev;
 
+    // Allow interaction between nodes
+    friend Node;
+
 public:
 
     // Returns the value of the node
@@ -26,19 +29,82 @@ public:
     // Returns a pointer to the previous node
     Node* prev(){ return this->_prev; }
 
-    // Set the pointer to the next node
-    void setNext(Node *next){ this->_next = next; }
-
-    // Set the pointer to the previous node
-    void setPrev(Node *prev){ this->_prev = prev; }
-
-    Node(int value, Node *next, Node *prev)
+    // Construct and bind to next and previous nodes
+    Node(int value, Node *prev=nullptr, Node *next=nullptr)
     {
         this->_value = value;
         this->_next = next;
         this->_prev = prev;
+        if(this->_next != nullptr) this->_next->_prev = this;
+        if(this->_prev != nullptr) this->_prev->_next = this;
     }
-    ~Node();
+
+    // Destroy and unbind from next or previous node (or bind them together)
+    ~Node()
+    {
+        if(this->_next != nullptr && this->_prev != nullptr)
+        {
+            this->_next->_prev = this->_prev;
+            this->_prev->_next = this->_next;
+        }
+        else if (this->_next != nullptr)
+        {
+            this->_next->_prev = nullptr;
+        }
+        else if (this->_prev != nullptr)
+        {
+            this->_prev->_next = nullptr;
+        }
+    }
+};
+
+class Queue
+{
+private:
+
+    // Pointer to the first node on the queue
+    Node *_front = nullptr;
+
+    // Pointer to the last node on the queue
+    Node *_back = nullptr;
+
+    size_t _size = 0;
+
+public:
+
+    // Returns the current size of the queue
+    const size_t size(){ return this->_size; }
+
+    // Returns whether the queue is empty or not
+    const bool empty(){ return this->_size == 0; }
+
+    // Returns the value of the node at the front of the queue
+    const int front(){ return this->_front->value(); }
+
+    // Returns the value of the node at the back of the queue
+    const int back(){ return this->_back->value(); }
+
+    // Add a node to the back of the queue
+    void push_back(int value)
+    {
+        Node *new_node = new Node(value, this->_back);
+        if(this->_size == 0) this->_front = new_node;
+        this->_back = new_node;
+        this->_size++;
+    }
+
+    int pop_front()
+    {
+        Node *old_node = this->_front;
+        this->_front = this->_front->next();
+        int value = old_node->value();
+        delete old_node;
+        this->_size--;
+        return value;
+    }
+
+    Queue();
+    ~Queue(){ while(!this->empty()) pop_front(); }
 };
 
 int main(){}
